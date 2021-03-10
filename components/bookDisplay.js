2d9d82f70d7917493b2f5d6a21dadc9f6c7c300b
mstovy <micahstovall@Micahs-MacBook-Pro-2.local>
2021-03-10 15:38:19 -0700

    debugged and working locally

--- components/bookDisplay.js
+++ components/bookDisplay.js
@@ -9,9 +9,9 @@ app.component('book-display', {
     template:
         /*html*/
         ` <ul class="col-sm-12 col-md-6 col-xl-4">
-        <a class="list-group-item list-group-item-action active" :href="this.BookObj.selfLink"> {{ this.BookObj.volumeInfo.title }} </a >
-        <li class= "list-group-items" > {{ this.BookObj.volumeInfo.authors }}</li >
-        <li class= "list-group-item" > {{ this.BookObj.volumeInfo.publisher }}, {{ this.BookObj.volumeInfo.publishedDate }}</li >
+        <a class="list-group-item list-group-item-action active" :href="this.bookObj.selfLink"> {{ this.bookObj.volumeInfo.title }} </a >
+        <li class= "list-group-items" > {{ this.bookObj.volumeInfo.authors }}</li >
+        <li class= "list-group-item" > {{ this.bookObj.volumeInfo.publisher }}, {{ this.bookObj.volumeInfo.publishedDate }}</li >
         </ul > `,
     computed: {
         bookObj() {
